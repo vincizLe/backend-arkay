bc58e9ea04a6ada15edfbff26274a95d69b26249
Eduardo Lenin Ortega Esquivel <eduardo.ortega@arisale.com.pe>
2023-05-17 22:49:33 -0500

    Feature unit was implemented successfully

--- src/features/unit/domain/use-cases/save-unit.use-case.ts
+++ src/features/unit/domain/use-cases/save-unit.use-case.ts
@@ -14,12 +14,11 @@ export class SaveUnitUseCase {
 		for (const unitEnum of UnitEnum.toArray()) {
 			const template = await this.templateRepository.getOne(unitEnum)
 
-			console.log('template -> ', template)
 			const unit = Unit.create({
 				name: unitEnum,
 				session: Session.create({
 					name: template.sessionTemplate.sessionName,
-					videoUrl: template.sessionTemplate.videoUrl,
+					videoUrl: template.sessionTemplate?.videoUrl ?? null,
 					isCompleted: false
 				}),
 				assessment: Assessment.create({
